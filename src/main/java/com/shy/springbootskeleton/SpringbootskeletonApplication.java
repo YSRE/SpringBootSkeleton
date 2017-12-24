988023b053e18cf78602b2e7eb358b59f5a98277
a4223676 <270794583@qq.com>
2017-12-24 09:40:14 +0800

    使用springBoot搭建分布式框架. 项目初始化

---
+++ src/main/java/com/shy/springbootskeleton/SpringbootskeletonApplication.java
@@ -0,0 +1,12 @@
+package com.shy.springbootskeleton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootskeletonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootskeletonApplication.class, args);
+	}
+}
---
+++ src/test/java/com/shy/springbootskeleton/SpringbootskeletonApplicationTests.java
@@ -0,0 +1,16 @@
+package com.shy.springbootskeleton;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootskeletonApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
